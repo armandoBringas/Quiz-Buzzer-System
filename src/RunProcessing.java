d16ef0cc2c28c079394bcb9fcbbd32ecf449e5a3
armandobringas <armando.bringas@gmail.com>
2019-01-27 01:28:54 -0600

    Add example for communication with processin and arduino

--- src/RunProcessing.java
+++ src/RunProcessing.java
@@ -1,21 +1,34 @@
 import processing.core.PApplet;
 import processing.core.PFont;
+import processing.serial.*;
 
 public class RunProcessing extends PApplet{
+
+    PFont font;
+    Serial myPort;  // Create object from Serial class
+    String val;     // Data received from the serial port
 
     public static void main(String... args){
         PApplet.main("RunProcessing");
     }
 
-    PFont font;
 
     public void settings(){
         size(500, 500);
+        String portName = Serial.list()[2]; //change the 0 to a 1 or 2 etc. to match your port
+        myPort = new Serial(this, portName, 9600);
     }
 
     public void draw(){
 
         background(100,100,100);
+
+        // read arduino port
+        if ( myPort.available() > 0)
+        {  // If data is available,
+            val = myPort.readStringUntil('\n');         // read it and store it in val
+        }
+        println(val); //print it out in the console
 
         // font setup
         font = loadFont("AgencyFB-Bold-200.vlw");
