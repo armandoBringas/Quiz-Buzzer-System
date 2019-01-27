c8ef45e1baa3e8d98743fc6d712fa158f7866efd
armandobringas <armando.bringas@gmail.com>
2019-01-27 00:10:01 -0600

    Loading font data for processing sketch

--- src/RunProcessing.java
+++ src/RunProcessing.java
@@ -1,17 +1,34 @@
 import processing.core.PApplet;
+import processing.core.PFont;
 
 public class RunProcessing extends PApplet{
 
+    public static void main(String... args){
+        PApplet.main("RunProcessing");
+    }
+
+    PFont font;
+
     public void settings(){
-        size(200, 200);
+        size(500, 500);
     }
 
     public void draw(){
-        background(0);
-        ellipse(mouseX, mouseY, 20, 20);
-    }
 
-    public static void main(String... args){
-        PApplet.main("RunProcessing");
+        background(100,100,100);
+
+        // font setup
+        font = loadFont("AgencyFB-Bold-200.vlw");
+        textFont(font);
+        textAlign(CENTER);
+        textSize(100);
+        text("Trivia Game", width/2, 100);
+        textSize(150);
+
+        // timer setup
+        int s = second();
+        int m = minute();
+        int h = hour();
+        text(h+":"+m+":"+s, width/2, (height/2)+50);
     }
 }
