5dee0f6ea8cd189637da67e3df24c6caeda129cf
armandobringas <armando.bringas@gmail.com>
2019-02-28 19:23:43 -0600

    Fix project structure

---
+++ src/LaunchGame.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class LaunchGame extends PApplet{
+
+    public static void main(String... args){
+        PApplet.main("LaunchGame");
+    }
+
+    public void settings(){
+        size(500, 500);
+    }
+
+    public void draw(){
+        background(100,100,100);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+        LaunchGame executeGame = new LaunchGame();
+        executeGame.main();
+    }
+
+}
